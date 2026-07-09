b95eb1f84994c22821539678d8e2e84ab679a0e0
myje0804 <myje0804@gmail.com>
2026-07-09 15:08:48 +0900

    Add Docker 설정 (Dockerfile, docker-compose.yml)

--- SWIDEA_SITE/config/settings.py
+++ SWIDEA_SITE/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,6 +69,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
@@ -75,9 +78,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MYSQL_DATABASE", "swidea_db"),
+        "USER": os.environ.get("MYSQL_USER", "swidea_user"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "swidea_password"),
+        "HOST": os.environ.get("MYSQL_HOST", "swidea-db"),
+        "PORT": os.environ.get("MYSQL_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
@@ -118,7 +128,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
